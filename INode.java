8ffea5eca9b6a9a562704ce13537ca05cd749ad0
rishika251 <rishikasadineni@gmail.com>
2022-11-20 12:21:18 +0530

    Added:Ability to create tree with multiple elements

---
+++ INode.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.day15;
+
+public class INode<K> {
+	 public K getKey() {
+		return null;
+	}
+
+	    void setKey(K key) {
+		}
+
+	    void setNext(INode next) {
+		}
+
+	    INode getNext() {
+			return null;
+		}
+
+	}
+
