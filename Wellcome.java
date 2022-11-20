785b21d9395bdf706fe6956a8f6231fa4fe9857a
rishika251 <rishikasadineni@gmail.com>
2022-11-20 10:54:06 +0530

    Added:Wellcome

---
+++ Wellcome.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day15;
+
+public class Wellcome {
+
+	public static void main(String[] args) {
+		System.out.println("Wellcome to tree and hashtableprograms");
+		
+
+	}
+
+}
